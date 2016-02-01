79f4adafe68262b1937f6639843d1958241774ad
Sallust <daniyom@gmail.com>
2016-02-01 15:57:31 -0500

    set file structure

---
+++ src/js/models/food.js
@@ -0,0 +1,21 @@
+var app = app || {};
+
+
+//I'm the food model, I'm the data surrounding one added food
+
+console.log ('Hi from the food model');
+
+app.Food = Backbone.Model.extend({
+	//default food properties
+	defaults: {
+		name: 'Food',
+		calories: 200,
+		protein: 4,
+		fat: 2,
+		added: false
+	}
+
+	//TODO What additional logic should live here?
+	//Will user be changing data somehow?
+
+})
