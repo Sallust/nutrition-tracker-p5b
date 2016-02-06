8567719f3cebc94b22e81500ebd14f646a85e949
Sallust <daniyom@gmail.com>
2016-02-05 20:57:03 -0500

    bug: Date work- adjust generate file name for GMT diff
    -in lieu of writing a formula to generate the file names

--- src/js/models/date.js
+++ src/js/models/date.js
@@ -8,9 +8,6 @@ var app = app || {};
 
 var DateModel = Backbone.Model.extend({
 	defaults: {
-		//TODAY: new Date(Date.now()),
-		//dayInFocus: new Date(Date.now()),
-		//todayInFocus: true,
 		tmrw: '',
 		yesterday: '',
 		todayFileStr:'',
@@ -18,55 +15,33 @@ var DateModel = Backbone.Model.extend({
 		yesterdayFileStr:''
 	},
 	initialize: function() {
-		var oneDay = 86400000
 		var d = new Date(Date.now());
-		var UTC = d.valueOf();
-
-		var tmrw = new Date(UTC + oneDay);
-		var yesterday = new Date(UTC - oneDay);
-
-
-		//var todayFileStr = d.toJSON().slice(0,10);
-		//var tmrwFileStr = tmrw.toJSON().slice(0,10);
-		//var yesterdayFileStr = yesterday.toJSON().slice(0,10);
-
-
 		this.set({
 			todayDateStr: d.toDateString(),
 			dayInFocus: d,
-			//tmrw: new Date(UTC + oneDay),
-//			yesterday: new Date(UTC - oneDay),
-//			todayFileStr: d.toJSON().slice(0,10),
-//			tmrwFileStr: tmrw.toJSON().slice(0,10),
-//			yesterdayFileStr: yesterday.toJSON().slice(0,10)
-		})
+		});
 
-
-
-		//this.set('dayInFocus', new Date(Date.now()));
 		this.setRelativeDay(0)
 	},
 	setRelativeDay: function(direction) {
-		var currentDay = this.get('dayInFocus')
+		var currentDay = this.get('dayInFocus');
 		currentDay.setDate(currentDay.getDate() + direction);
+
 		var UTC = currentDay.valueOf();
-
 		var oneDay = 86400000;
-
 		var tmrw = new Date(UTC + oneDay);
 		var yesterday = new Date(UTC - oneDay);
-
+		var GMT = new Date(UTC - 18000000 );  //milliseconds in 5 hrs; so toJSON returns todays date in a clean format
 
 		this.set({
 			dayInFocus: currentDay,
 			tmrw: new Date(UTC + oneDay),
 			yesterday: new Date(UTC - oneDay),
-			todayFileStr: currentDay.toJSON().slice(0,10),
+			todayFileStr: GMT.toJSON().slice(0,10),
 			yesterdayDateStr: yesterday.toDateString(),
 			focalDateStr: currentDay.toDateString(),
 			tmrwDateStr: tmrw.toDateString(),
 			todayInFocus: currentDay.toDateString() === this.get('todayDateStr') ? true : false
-
 		})
 
 		//this.set('todayInFocus', this.get('todayDateStr') === this.get('focalDateStr') ? true : false)
